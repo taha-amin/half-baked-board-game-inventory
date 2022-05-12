f6691e10f470c64564f2779bb536487e769a2ef3
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-12 14:45:18 -0700

    refactored code in listpage.js from functional components into class components

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,28 +1,41 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 import { getGames } from './services/fetch-utils';
 import Game from './Game';
 
-export default function ListPage() {
+export default class ListPage extends React.Component {
   // you'll need some state to hold onto the array of games
-  const [games, setGames] = useState([]);
+  constructor() {
+    super();
+    this.state = {
+      games: [],
+    };
+  }
+
+  async componentDidMount() {
+    const fetchedGames = await getGames();
+
+    this.setState({ games: fetchedGames });
+  }
 
   // fetch the games on load and inject them into state
-  useEffect(() => {
-    async function fetch() {
-      const fetchedGames = await getGames();
+  // useEffect(() => {
+  //   async function fetch() {
+  //     const fetchedGames = await getGames();
 
-      setGames(fetchedGames);
-    }
+  //     setGames(fetchedGames);
+  //   }
 
-    fetch();
-  }, []);
+  //   fetch();
+  // }, []);
 
-  return (
-    <div className="list games">
-      {/* map through the games in state and render Game components */}
-      {games.map((game) => (
-        <Game key={game.id} game={game} />
-      ))}
-    </div>
-  );
+  render() {
+    return (
+      <div className="list games">
+        {/* map through the games in state and render Game components */}
+        {this.maps.map((game) => (
+          <Game key={game.id} game={game} />
+        ))}
+      </div>
+    );
+  }
 }
